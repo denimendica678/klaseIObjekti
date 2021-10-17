a1e429be0fee3da62fbba4a6d6ef6b8efa5c49cb
denimendica678 <58472463+denimendica678@users.noreply.github.com>
2021-10-17 17:01:20 +0200

    Add files via upload

---
+++ geometrijskiLikKlaseIObjekti/geometrijskiLikKlaseIObjekti/GeometrijskiLik.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace geometrijskiLikKlaseIObjekti
+{
+    public class GeometrijskiLik
+
+    {
+        int BrojStranica;
+        private double[] stranice = new double[10];
+
+        public int BrojStranica1 { get => BrojStranica; set => BrojStranica = value; }
+        public double[] Stranice { get => stranice; set => stranice = value; }
+
+        public GeometrijskiLik(int brojStranica, double[] stranice)
+        {
+            BrojStranica = brojStranica;
+            Stranice = stranice;
+        }
+
+
+
+        static void UcitajStranice()
+        {
+
+        }
+
+            public double Opseg()
+            {
+                double opseg=0;
+
+                foreach(double broj in Stranice)
+                {
+                    opseg = broj + opseg;
+                }
+
+                return opseg;
+        }
+
+    }
+
+    class Trokut : GeometrijskiLik
+    {
+        public Trokut(int brojStranica, double[] stranice) : base(brojStranica, stranice)
+        {
+
+        }
+
+
+    }
+
+    class Cetverokut : GeometrijskiLik
+    {
+        public Cetverokut(int brojStranica, double[] stranice) : base(brojStranica, stranice)
+        {
+            Stranice = new double[brojStranica];
+            for (int i = 0; i <= 4; i++)
+            {
+                Console.Write("Unesite brojeve stranica:");
+                Stranice[i] = Convert.ToDouble(Console.ReadLine());
+
+            }
+        }
+    }
+}
