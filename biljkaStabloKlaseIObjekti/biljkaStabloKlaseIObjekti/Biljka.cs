63554ed5f5d6486671b04df745e7a980afad6b04
denimendica678 <58472463+denimendica678@users.noreply.github.com>
2021-10-17 17:00:43 +0200

    Add files via upload

---
+++ biljkaStabloKlaseIObjekti/biljkaStabloKlaseIObjekti/Biljka.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace biljkaStabloKlaseIObjekti
+{
+    public class Biljka
+    {
+        public class Cvijet : Biljka
+        {
+            public Cvijet()
+            {
+            }
+        }
+
+        public class Stablo : Biljka
+        {
+             bool opadajuListovi;
+
+
+            public Stablo(bool opadajuListovi) => this.opadajuListovi = opadajuListovi;
+
+            public bool OpadajuListovi1 { get => opadajuListovi; set => opadajuListovi = value; }
+
+            public static void OpadajuListovi()
+            {
+                
+            }
+
+            public class Bijelogoricno : Stablo
+            {
+                public Bijelogoricno(bool opadajuListovi) : base(opadajuListovi)
+                {
+                    OpadajuListovi = opadajuListovi;
+                }
+
+                public bool OpadajuListovi { get; }
+            }
+
+            public class Crnogoricno : Stablo
+            {
+                
+
+                public Crnogoricno(bool opadajuListovi) : base(opadajuListovi)
+                {
+                    OpadajuListovi = opadajuListovi;
+                }
+
+                public bool OpadajuListovi { get; }
+            }
+
+        }
+    }
+}
---
+++ biljkaStabloKlaseIObjekti/biljkaStabloKlaseIObjekti/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace biljkaStabloKlaseIObjekti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Biljka.Stablo.Bijelogoricno bijelogoricno = new Biljka.Stablo.Bijelogoricno(true);
+            Biljka.Stablo.Bijelogoricno Hrast = bijelogoricno;
+
+            Biljka.Stablo.Crnogoricno crnogoricno = new Biljka.Stablo.Crnogoricno(true);
+            Biljka.Stablo.Crnogoricno Bor = crnogoricno;
+
+            Console.WriteLine("Bijelogorično je hrast: " + Hrast.OpadajuListovi1);
+            Console.WriteLine("Crnogorično je bor: " + Bor.OpadajuListovi1);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
